ba5b68765b55cf858d7736c52a5192fb2f5d9d26
Drew Devero <deverobelfon@gmail.com>
2020-11-17 15:00:19 -0500

    cleaned up my javascript code, eliminating repetition of declaring DOM changes

--- docs/scripts/shield.js
+++ docs/scripts/shield.js
@@ -10,16 +10,18 @@ const POINT5 = document.getElementById("pointFive");
 })();
 
 // sizeShift function - makes each word located around the shield polygon re-size and change color, one at a time, in a clockwise sequence
-
+function pointAnimation(point) { 
+    point.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)
+}
 (function sizeShift() {
-    setTimeout(() => {POINT1.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 2000);
-    setTimeout(() => {POINT2.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 4000);
-    setTimeout(() => {POINT3.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 6000);
-    setTimeout(() => {POINT4.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 8000);
-    setTimeout(() => {POINT5.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 10000);
-    setTimeout(() => {setInterval(() => {POINT1.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 10000)}, 2000);
-    setTimeout(() => {setInterval(() => {POINT2.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 10000)}, 4000);
-    setTimeout(() => {setInterval(() => {POINT3.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 10000)}, 6000);
-    setTimeout(() => {setInterval(() => {POINT4.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 10000)}, 8000);
-    setTimeout(() => {setInterval(() => {POINT5.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)}, 10000)}, 10000);
+    setTimeout(() => pointAnimation(POINT1), 2000);
+    setTimeout(() => pointAnimation(POINT2), 4000);
+    setTimeout(() => pointAnimation(POINT3), 6000);
+    setTimeout(() => pointAnimation(POINT4), 8000);
+    setTimeout(() => pointAnimation(POINT5), 10000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINT1), 10000)}, 2000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINT2), 10000)}, 4000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINT3), 10000)}, 6000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINT4), 10000)}, 8000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINT1), 10000)}, 10000);
 })();
