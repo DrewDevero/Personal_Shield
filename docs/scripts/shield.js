0b20b851a62b6caaa68a1e903b76449e5db2027b
Drew Devero <deverobelfon@gmail.com>
2020-11-18 16:08:59 -0500

    added templates and per Angel's suggestion, used class to link to my DOM elements

--- docs/scripts/shield.js
+++ docs/scripts/shield.js
@@ -1,9 +1,14 @@
 const SHIELD = document.getElementById("shieldSection");
-const POINT1 = document.getElementById("pointOne");
-const POINT2 = document.getElementById("pointTwo");
-const POINT3 = document.getElementById("pointThree");
-const POINT4 = document.getElementById("pointFour");
-const POINT5 = document.getElementById("pointFive");
+const POINTS = document.getElementsByClassName("points");
+
+/* (function pointAnimation(points) {
+for (let i = 0; i < POINTS.length; i++) {
+    let time = 2000;
+    setTimeout(() => points[i].animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000), time);
+    setTimeout(() => {setInterval(() => points[i].animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000), 10000)}, time);
+    time++;
+}
+})(POINTS); */
 
 (function phase() {
         SHIELD.style.opacity = "1";
@@ -14,14 +19,14 @@ function pointAnimation(point) {
     point.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)
 }
 (function sizeShift() {
-    setTimeout(() => pointAnimation(POINT1), 2000);
-    setTimeout(() => pointAnimation(POINT2), 4000);
-    setTimeout(() => pointAnimation(POINT3), 6000);
-    setTimeout(() => pointAnimation(POINT4), 8000);
-    setTimeout(() => pointAnimation(POINT5), 10000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINT1), 10000)}, 2000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINT2), 10000)}, 4000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINT3), 10000)}, 6000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINT4), 10000)}, 8000);
-    setTimeout(() => {setInterval(() => pointAnimation(POINT5), 10000)}, 10000);
+    setTimeout(() => pointAnimation(POINTS[0]), 2000);
+    setTimeout(() => pointAnimation(POINTS[1]), 4000);
+    setTimeout(() => pointAnimation(POINTS[2]), 6000);
+    setTimeout(() => pointAnimation(POINTS[3]), 8000);
+    setTimeout(() => pointAnimation(POINTS[4]), 10000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINTS[0]), 10000)}, 2000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINTS[1]), 10000)}, 4000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINTS[2]), 10000)}, 6000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINTS[3]), 10000)}, 8000);
+    setTimeout(() => {setInterval(() => pointAnimation(POINTS[4]), 10000)}, 10000);
 })();
