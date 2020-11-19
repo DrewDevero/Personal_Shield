341d35b984d6fcf7d3a1c65edb2969fb2dac03f5
Drew Devero <deverobelfon@gmail.com>
2020-11-19 10:43:11 -0500

    removed logging that was placed for testing

--- docs/scripts/shield.js
+++ docs/scripts/shield.js
@@ -8,12 +8,13 @@ const POINTS = document.getElementsByClassName("points");
 function pointAnimation(point) { 
     point.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)
 }
+
 // sizeShift function - makes each word located around the shield(id="shield") re-size and change color, one at a time, in a clockwise sequence
+
 (function sizeShift(time) {
 for (let i = 0; i < POINTS.length; i++) {
     setTimeout(() => pointAnimation(POINTS[i]), time);
     setTimeout(() => {setInterval(() => pointAnimation(POINTS[i]), 10000)}, time);
     time += 2000;
-    console.log(time);
     }
 })(2000);
