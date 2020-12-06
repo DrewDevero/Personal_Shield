1f60e85c50afef28ed5a35427c1d1caa24378215
Drew Devero <deverobelfon@gmail.com>
2020-12-06 03:36:04 -0500

    added media query for cross-device optimization

--- docs/scripts/shield.js
+++ docs/scripts/shield.js
@@ -6,7 +6,11 @@ const POINTS = document.getElementsByClassName("points");
 })();
 
 function pointAnimation(point) { 
-    point.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)
+    if (window.screen.width <= 500 || document.body.clientWidth <= 500) {
+        point.animate([{fontSize: "1rem", easing: "ease-in-out"},{fontSize: "1.5rem", color: "blue", easing: "ease-in-out"},{fontSize: "1rem", easing: "ease-in-out"}], 2000)
+    } else {
+        point.animate([{fontSize: "inherit", easing: "ease-in-out"},{fontSize: "3rem", color: "blue", easing: "ease-in-out"},{fontSize: "inherit", easing: "ease-in-out"}], 2000)
+    }
 }
 
 // sizeShift function - makes each word located around the shield(id="shield") re-size and change color, one at a time, in a clockwise sequence
